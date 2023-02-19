f02f3546589adf5d2a32c7461de14d11ef3f7d59
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2023-02-19 18:15:32 +0300

    task1

--- Less_2/Less2.java
+++
@@ -1,5 +0,0 @@
-package Less_2;
-
-public class Less2 {
-    
-}
---
+++ Less_2/Task1.java
@@ -0,0 +1,19 @@
+package Less_2;
+
+public class Task1 {
+    public static void main(String[] args){
+        String str = "шалаш";
+        if (str.length() == 0){
+            System.out.println("Строка пустая");
+            return;
+        }
+        for (int i = 0; i<str.length()/2; i++) {
+            if (str.charAt(i) !=str.charAt(str.length()-1 -i)) {
+                System.out.println("Строка не палиндром");
+                return;
+            }
+        }
+        System.out.println("Строка палиндром");
+        
+    }
+}
